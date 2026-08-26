31338c79a9af67742c1e930365bd21bae559bfc2
LogicCodeWhisperer <chasestephenson79@yahoo.com>
2026-08-26 15:02:55 -0400

    Commit 3

--- Ch2/IntegerDemo.java
+++ Ch2/IntegerDemo.java
@@ -1,21 +1,23 @@
 // Program: DataDemo.java
 // Author: Chase Stephenson
 // Date Written 8/26/2026
- 
+
 public class IntegerDemo
 {
     public static void main (String[] args)
     {
         //Declarations
         int anInt = 1234;
-        byte aByte = 1234;
-        short aShort = 1234;
-        long aLong = 1234;
+        byte aByte = 12;
+        short aShort = 12345;
+        long aLong = 1234567890987654321L;
+        int anotherInt = anInt * 10000000;
 
         //Output statements
-        System.out.println("The int is   " + anInt);
-        System.out.println("The byte is  " + aByte);
-        System.out.println("The short is " + aShort);
-        System.out.println("The long is  " + aLong);
+        System.out.println("The int is    " + anInt);
+        System.out.println("The byte is   " + aByte);
+        System.out.println("The short is  " + aShort);
+        System.out.println("The long is   " + aLong);
+        System.out.println("Another int is " + anotherInt);
     }
 }
