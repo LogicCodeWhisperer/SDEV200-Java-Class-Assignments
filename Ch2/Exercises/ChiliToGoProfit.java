1b9b76abbde243406f1131e002128db1a847a509
LogicCodeWhisperer <chasestephenson79@yahoo.com>
2026-08-27 12:15:59 -0400

    Profit Calculations of Meals

--- Ch2/Exercises/ChiliToGoProfit.java
+++ Ch2/Exercises/ChiliToGoProfit.java
@@ -1,6 +1,6 @@
-// Program: ChiliToGo.java
+// Program: ChiliToGoProfit.java
 // Author: Chase Stephenson
-// Date Written 8/26/2026
+// Date Written 8/27/2026
 
 import java.util.Scanner;
 public class ChiliToGoProfit 
@@ -8,13 +8,13 @@ public class ChiliToGoProfit
     public static void main(String[] args)
     {
         //Declarations
-        int adultMealPrice = 7;
-        int childMealPrice = 4;
-        int numAdultMeals;
-        int numChildMeals;
-        int adultMealTotal;
-        int childMealTotal;
-        int total;
+        double adultMealPrice = 7;
+        double childMealPrice = 4;
+        double numAdultMeals;
+        double numChildMeals;
+        double adultMealTotalProfit;
+        double childMealTotalProfit;
+        double totalProfit;
         Scanner input = new Scanner(System.in);
 
         //Initial Informative output statment
@@ -22,19 +22,24 @@ public class ChiliToGoProfit
 
         //User Input
         System.out.print("How many adult meals? >> ");
-        numAdultMeals = input.nextInt();
+        numAdultMeals = input.nextDouble();
         System.out.print("How many child meals? >> ");
-        numChildMeals = input.nextInt();
+        numChildMeals = input.nextDouble();
         System.out.println();
         
-        //Calculations
-        adultMealTotal = adultMealPrice * numAdultMeals;  //Total for adult meals
-        childMealTotal = childMealPrice * numChildMeals;  //Total for child meals
-        total = adultMealTotal + childMealTotal;          //Total for all meals
+        //Profit Calculations
+        adultMealTotalProfit = (adultMealPrice - 4.35) * numAdultMeals;  //Total profit for adult meals
+        childMealTotalProfit = (childMealPrice - 3.10) * numChildMeals;  //Total profit for child meals
+        totalProfit = adultMealTotalProfit + childMealTotalProfit;       //Total profit for all meals
+
+        //Rounding Calculation
+        adultMealTotalProfit = Math.round(adultMealTotalProfit * 100.00) / 100.00;
+        childMealTotalProfit = Math.round(childMealTotalProfit * 100.00) / 100.00;
+        totalProfit = Math.round(totalProfit * 100.00) / 100.00;
 
         //Output Statements
-        System.out.println("Total cost for adult meals is $" + adultMealTotal);
-        System.out.println("Total cost for child meals is $" + childMealTotal);
-        System.out.println("Total cost for all meals is $" + total);
+        System.out.printf("Total Profit for adult meals is $%.2f\n", adultMealTotalProfit);
+        System.out.printf("Total Profit for child meals is $%.2f\n", childMealTotalProfit);
+        System.out.printf("Total Profit for all meals is $%.2f\n", totalProfit);
     }
 }
