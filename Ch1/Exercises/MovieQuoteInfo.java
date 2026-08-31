9a4ef62760d7ab4fc9f721485c4057d9efab04c3
LogicCodeWhisperer <chasestephenson79@yahoo.com>
2026-08-31 15:36:47 -0400

    Rearranged folder structure for easier navigation

---
+++ Ch1/Exercises/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+/* 
+   Program: MovieQuoteInfo.java
+   Author: Chase Stephenson
+   Date Written:
+*/
+public class MovieQuoteInfo 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Quote:    \"Can I refill your eggnog for you? Get you something to eat? Drive you out to the middle of nowhere and leave you for dead?\"");
+        System.out.println("Movie:     National Lampoon's Christmas Vacation");
+        System.out.println("Character: Clark Griswold");
+        System.out.println("Year:      1989");
+    }
+}
