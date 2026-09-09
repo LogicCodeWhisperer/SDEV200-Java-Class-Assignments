a6ff7025ccb912bc1b2f92c6ed7f6f12adbabb80
LogicCodeWhisperer <chasestephenson79@yahoo.com>
2026-09-09 11:39:39 -0400

    Fixed input "leak"

--- Ch3/YouDoIt/Programs/ParadiseInfo2.java
+++ Ch3/YouDoIt/Programs/ParadiseInfo2.java
@@ -23,6 +23,7 @@ public class ParadiseInfo2
         System.out.println("Special this week on any service over " + price);
         System.out.println("Discount of " + discount + " percent");
         System.out.println("That's a savings of at least $" + savings);
+        keyboard.close();
     }
 
     public static void displayInfo()
